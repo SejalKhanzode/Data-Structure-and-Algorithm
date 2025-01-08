5c80d09784464d9d10163a44bf0b3862f2a2540f
Sejal Khanzode <khanzodesejal@gmail.com>
2025-01-08 15:54:11 +0530

    8/1/25

---
+++ Day1/HollowPyramid.java
@@ -0,0 +1,26 @@
+package Day1;
+
+public class HollowPyramid {
+    public static void main(String[] args) {
+        int n=5;
+        for (int i = 0; i < n; i++) {
+            int k=0;
+            for (int j = 0; j < ((2*n)-1); j++) {
+                if(j<n-i-1){
+                    System.out.print(" ");
+                }else if(k<2*i+1){
+                    if(k==0 || k==2*i || i==n-1)
+                       System.out.print("*");
+                    else
+                       System.out.print(" ");
+                    k++;
+                }
+               
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();    
+        }
+    }
+}
